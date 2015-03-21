33f05a68e1d47ee04833c27ad37a12bd3ec44035
Maxim Seliverstov <maxim.web.developer@gmail.com>
2015-03-22 02:45:44 +0300

    Init commit.

---
+++ wp_mu_recent_post_widget.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Recent post from sub site
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: wp_mu_recent_post_widget
+Domain Path: /languages
+*/
+
