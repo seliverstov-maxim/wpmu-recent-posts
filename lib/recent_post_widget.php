d8ce82abe8d71af1784ea6a7444d40753417d9f0
Maxim Seliverstov <maxim.web.developer@gmail.com>
2015-03-22 05:04:34 +0300

    Create widget, add widget to admin panel.

---
+++ lib/recent_post_widget.php
@@ -0,0 +1,39 @@
+<?php
+class RecentPostWidget extends WP_Widget {
+  function __construct() {
+    parent::__construct('recent_post_widget',
+      __('Recent Post Widget', 'recent_post_widget'),
+      array(
+        'description' => __('Recent post from sub site', 'recent_post_from_subsite')
+      )
+    );
+  }
+
+  function widget($args, $instance) {
+  }
+
+  function form($instance) {
+    global $wpdb;
+    $sites = wp_get_sites(array(
+      'network_id' => $wpdb->siteid,
+      'public' => true));
+?>
+    <p>
+      <label>SubSite</label>
+      <select name='subsite' />
+        <?php
+          foreach($sites as $val) {
+            $id = $val['blog_id'];
+            $details = get_blog_details($id);
+            echo "<option value=$id >".$details->blogname."</option>";
+          }
+        ?>
+      </select>
+    </p>
+<?php
+  }
+
+  function update($new_instance, $old_instance) {
+  }
+}
+?>
--- wp_mu_recent_post_widget.php
+++ wp_mu_recent_post_widget.php
@@ -2,11 +2,20 @@
 /*
 Plugin Name: Recent post from sub site
 Version: 0.1-alpha
-Description: PLUGIN DESCRIPTION HERE
-Author: YOUR NAME HERE
-Author URI: YOUR SITE HERE
-Plugin URI: PLUGIN SITE HERE
+Description: This plugin outputs in widget recent post from subsites of multisite.
+Author: Maxim Seliverstov
+Author URI: github.com/seliverstov-maxim
+Plugin URI: github.com/seliverstov-maxim/wpmu-recent-posts
 Text Domain: wp_mu_recent_post_widget
 Domain Path: /languages
 */
 
+$base = plugin_dir_path(__FILE__);
+require_once(join(DIRECTORY_SEPARATOR, array($base, 'lib', 'recent_post_widget.php')));
+
+add_action('widgets_init', 'register_recent_post_widget');
+function register_recent_post_widget() {
+  if(is_multisite()){
+    register_widget('RecentPostWidget');
+  }
+}
