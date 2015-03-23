7c46e9cafa0fd06871ba15c6dcda81ad24edfa5a
Maxim Seliverstov <maxim.web.developer@gmail.com>
2015-03-23 12:53:17 +0000

    Add frontend part. Fix admin form.

--- lib/recent_post_widget.php
+++ lib/recent_post_widget.php
@@ -10,6 +10,19 @@ class RecentPostWidget extends WP_Widget {
   }
 
   function widget($args, $instance) {
+    echo $args['before_widget'];
+    $blog_id = $instance['blog_id'];
+    switch_to_blog($blog_id);
+    $posts = wp_get_recent_posts(array('numberposts' => 1));
+    foreach($posts as $post) {
+      echo "<a href='" . get_permalink($post['ID']) . "'>";
+      echo "<div class='recent_post_title'>" . $post['post_title'] . "</div>";
+      echo "<div class='recent_post_thumbnail'>" . get_the_post_thumbnail( $post['ID'], 'thumbnail' ) . "</div>";
+      echo "<div class='recent_post_blogname'>" . get_bloginfo('name') . "</div>";
+      echo "</a>";
+    }
+    restore_current_blog();
+    echo $args['after_widget'];
   }
 
   function form($instance) {
@@ -20,12 +33,12 @@ class RecentPostWidget extends WP_Widget {
 ?>
     <p>
       <label>SubSite</label>
-      <select name='subsite'>
+      <select name=<?php echo $this->get_field_name('blog_id'); ?> class="widefat">
         <?php
           foreach($sites as $val) {
             $id = $val['blog_id'];
             $details = get_blog_details($id);
-            echo "<option value=$id >".$details->blogname."</option>";
+            echo "<option value=$id ". ((array_key_exists('blog_id', $instance) && $instance['blog_id']==$id) ? ' selected ' : '') .">".$details->blogname."</option>";
           }
         ?>
       </select>
@@ -34,6 +47,10 @@ class RecentPostWidget extends WP_Widget {
   }
 
   function update($new_instance, $old_instance) {
+    $instance = array();
+    $instance['blog_id'] = array_key_exists('blog_id', $new_instance) ? intval($new_instance['blog_id']) : 0;
+
+    return $instance;
   }
 }
 ?>
